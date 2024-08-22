cf7872f574b160a2ea1c2e57f72d2b4cf6c1f058
Harrison Sayre <hsayre42@gmail.com>
2024-08-21 23:51:57 -0400

    <Applied styling>

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,16 +1,29 @@
+import './styling.css';
+
 const Table = ({ sat }) => {
+
+  const sats = sat.map((data) => 
+    <tr key = {data.id}>
+      <td> {data.name}</td>
+      <td> {data.type}</td>
+      <td> {data.launchDate}</td>
+      <td> {data.operational? "Active" : "Inactive" }</td>
+    </tr>
+  );
+
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type of Satellite</th>
+        <th>Launch Date</th>
+        <th>Status</th>
+      </tr>
+      </thead>
+        <tbody>
+          {sats}
+        </tbody>
    </table>
   );
 };
